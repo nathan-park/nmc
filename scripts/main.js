ef87031e7f8f64e76c467bd58e87359377f8c954
nathan-park <nathanpark@ufl.edu>
2015-12-04 11:22:14 -0500

    Week 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,57 @@
 $(document).ready(function () {
 	$('.travis').hide();
 	$('#videosTravis').change(function () {
-	$('.travis').hide();
-	$('#'+$(this).val()).slideToggle();
-  });
+		$('.travis').hide();
+		$('#'+$(this).val()).slideToggle();
+	});
 	$('.kanye').hide();
 	$('#videosKanye').change(function () {
-	$('.kanye').hide();
-	$('#'+$(this).val()).slideToggle();
-  });
+		$('.kanye').hide();
+		$('#'+$(this).val()).slideToggle();
+	});
+	$('.childish').hide();
+	$('#videosChildish').change(function () {
+		$('.childish').hide();
+		$('#'+$(this).val()).slideToggle();
+	});
+	$('.rocky').hide();
+	$('#videosRocky').change(function () {
+		$('.rocky').hide();
+		$('#'+$(this).val()).slideToggle();
+	});
+	$('.kendrick').hide();
+	$('#videosKendrick').change(function () {
+		$('.kendrick').hide();
+		$('#'+$(this).val()).slideToggle();
+	});
+	$('.paak').hide();
+	$('#videosPaak').change(function () {
+		$('.paak').hide();
+		$('#'+$(this).val()).slideToggle();
+	});
+	$('.pnd').hide();
+	$('#videosPnd').change(function () {
+		$('.pnd').hide();
+		$('#'+$(this).val()).slideToggle();
+	});
+	$('.jhene').hide();
+	$('#videosJhene').change(function () {
+		$('.jhene').hide();
+		$('#'+$(this).val()).slideToggle();
+	});
+	$('.flylo').hide();
+	$('#videosFlylo').change(function () {
+		$('.flylo').hide();
+		$('#'+$(this).val()).slideToggle();
+	});
+	$('.jamie').hide();
+	$('#videosJamie').change(function () {
+		$('.jamie').hide();
+		$('#'+$(this).val()).slideToggle();
+	});
+	$('.grime').hide();
+	$('#videosGrime').change(function () {
+		$('.grime').hide();
+		$('#'+$(this).val()).slideToggle();
+	});
 });
